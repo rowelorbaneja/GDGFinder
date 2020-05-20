dc59e9c7ec1384f9d23fa636b97ee69f76e73bec
Rowel Orbaneja <rowel.orbaneja@ddp.kyocera.com>
2020-05-21 07:15:19 +0800

    Kotlin Android Fundamentals 10.2: Material Design, dimens, and colors

--- app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
+++ app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
@@ -17,12 +17,15 @@
 package com.example.android.gdgfinder
 
 import android.os.Bundle
+import android.transition.TransitionManager
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.android.gdgfinder.databinding.ActivityMainBinding
@@ -63,7 +66,7 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
             val toolBar = supportActionBar ?: return@addOnDestinationChangedListener
-            when (destination.id) {
+            when(destination.id) {
                 R.id.home -> {
                     toolBar.setDisplayShowTitleEnabled(false)
                     binding.heroImage.visibility = View.VISIBLE
--- app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
@@ -16,13 +16,14 @@
 
 package com.example.android.gdgfinder.add
 
+import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+
 import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.AddGdgFragmentBinding
 import com.google.android.material.snackbar.Snackbar
@@ -45,9 +46,9 @@ class AddGdgFragment : Fragment() {
         viewModel.showSnackBarEvent.observe(this, Observer {
             if (it == true) { // Observed state is true.
                 Snackbar.make(
-                        activity!!.findViewById(android.R.id.content),
-                        getString(R.string.application_submitted),
-                        Snackbar.LENGTH_SHORT // How long to display the message.
+                    activity!!.findViewById(android.R.id.content),
+                    getString(R.string.application_submitted),
+                    Snackbar.LENGTH_SHORT // How long to display the message.
                 ).show()
                 viewModel.doneShowingSnackbar()
             }
@@ -56,4 +57,5 @@ class AddGdgFragment : Fragment() {
         setHasOptionsMenu(true)
         return binding.root
     }
+
 }
